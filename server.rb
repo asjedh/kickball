0f9b7d9a2ff1d5eb040c1fd398b605e2bbeb7196
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-05-21 11:15:01 -0400

    Submission for Kickball Assignment

--- server.rb
+++ server.rb
@@ -3,40 +3,76 @@ require 'pry'
 require 'csv'
 
 
-helpers do
-  def parse_roster
-    players = []
+def parse_roster(file_path)
+  players = []
 
-    CSV.foreach('roster.csv',headers:true) do |row|
-      players << {first_name: row['first_name'],
-                  last_name: row['last_name'],
-                  position: row['position'],
-                  team: row['team']
-                }
-    end
+  CSV.foreach(file_path,headers:true, header_converters: :symbol) do |row|
+    players << row.to_hash
+  end
+  players
+end
 
-    players
+
+def find_players_on_team(team, parsed_csv_file)
+  all_players = parsed_csv_file
+
+  all_players.select do |player|
+    player if player[:team] == team
   end
 end
 
+def find_all_players_in_position(position, parsed_csv_file)
+  all_players = parsed_csv_file
+
+  all_players.select do |player|
+    player[:position] == position
+  end
+end
+
+def gather_all_teams(parsed_csv_file)
+  all_players = parsed_csv_file
+  teams = []
+  all_players.each do |player|
+    teams << player[:team] if !teams.include?(player[:team])
+  end
+  teams
+end
+
+def gather_all_positions(parsed_csv_file)
+  all_players = parsed_csv_file
+  positions = []
+  all_players.each do |player|
+    positions << player[:position] if !positions.include?(player[:position])
+  end
+  positions
+end
+
 get '/' do
-  "Hi"
+  parsed_roster = parse_roster('roster.csv')
+  @title = "Kickball League"
+  @teams = gather_all_teams(parsed_roster)
+  @positions = gather_all_positions(parsed_roster)
+  erb :index
 end
 
 get '/teams/:team' do
+  parsed_roster = parse_roster('roster.csv')
   @team = params[:team]
-  @players = parse_roster
-  @team_name_capitalized = @team.split(" ").each { |name| name.capitalize! }.join(" ")
-
-
+  @title = @team
+  @players = find_players_on_team(@team, parsed_roster)
   erb :teams
-
 end
 
 get '/positions/:position' do
+  parsed_roster = parse_roster('roster.csv')
   @position = params[:position]
-  @players = parse_roster
-  @position_cap = @position.split(" ").each { |name| name.capitalize! }.join(" ")
+  @title = @position
+  @players = find_all_players_in_position(@position, parsed_roster)
   erb :positions
+end
+
+get '/:something' do
+  @something = params[:something]
+  redirect '/'
 end
 
